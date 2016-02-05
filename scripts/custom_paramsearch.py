e68e637d27e45fc2876ee3e34d5638f4b5b6a782
Ubuntu <ubuntu@ip-172-31-32-50.us-west-2.compute.internal>
2016-02-05 11:18:49 +0000

    search3_script

--- scripts/custom_paramsearch.py
+++ scripts/custom_paramsearch.py
@@ -16,7 +16,7 @@ def cv_bymonth(xgbInput):
     Only cases in 2014 have sessions data and the last test case is on June 30.
     """
     
-    for i in range(5,7):
+    for i in range(0,7):
         condition = 'dac_year == 2014 & dac_month == @i & action_counts != -1'
         valid_mask = xgbInput.trainDf.index.isin(xgbInput.trainDf.query(condition).index)
         valid_indx = np.where(valid_mask)
@@ -34,24 +34,18 @@ xgbInput.split_data(update_trainDf=True)
 param = {'num_class': 12, 'silent': 1, 'objective': 'multi:softprob'}
 
 param_grid = {}
-param_grid['eta'] = [.20]
+param_grid['eta'] = [.16]
 param_grid['max_depth'] = [6]
-param_grid['subsample'] = [.5]
-param_grid['colsample_bytree'] = [1]
-results = {}
-err_out = {}
-nrounds = 50
-
-#set up dataframe to store cross-validation from each fold
-cv_train = pd.DataFrame()
-cv_error = pd.DataFrame()
+param_grid['subsample'] = [.7, .9, 1]
+param_grid['colsample_bytree'] = [.3, .6]
+nrounds = 100
 
 #set up dataframe to store mean/stdev. after cross validation
-cv_tofile = pd.read_pickle('cv_results/actions_e20/errors_search2.p')
+cv_tofile = pd.DataFrame()
 
 #set up dataframe to store the parameters used for cross validation
 col_names = list(param_grid.iterkeys())
-df_params = pd.read_pickle('cv_results/actions_e20/params_search2.p')    
+df_params = pd.DataFrame(columns = col_names)   
 
 for cnt, p in enumerate(list(ParameterGrid(param_grid))):
     print cnt
@@ -59,7 +53,9 @@ for cnt, p in enumerate(list(ParameterGrid(param_grid))):
 #store errors from each month by doing cv
     cv_train = pd.DataFrame()
     cv_valid = pd.DataFrame()
+    err_out = {}
     for train_indx, valid_indx in cv_bymonth(xgbInput):   
+        results = {}
         dtrain = xgb.DMatrix(xgbInput.train_X[train_indx], label = xgbInput.train_Y[train_indx],
                     missing = -1)
         dvalid = xgb.DMatrix(xgbInput.train_X[valid_indx], label = xgbInput.train_Y[valid_indx],
@@ -67,17 +63,17 @@ for cnt, p in enumerate(list(ParameterGrid(param_grid))):
         evallist = [(dtrain, 'train'), (dvalid, 'eval')]
         bst = xgb.train(param, dtrain, nrounds, evallist, feval = calc_ndcg.evalerror, evals_result = results)
         cv_train = pd.concat([cv_train, pd.Series(results['train']['error'])], axis = 1)
-        cv_error = pd.concat([cv_error, pd.Series(results['eval']['error'])], axis = 1)
-        
+        cv_valid = pd.concat([cv_valid, pd.Series(results['eval']['error'], name = str(len(cv_valid)))], axis = 1)
+
+    pd.to_pickle(cv_valid, 'cv_results/actions_e20/search3/res' + str(cnt) +'.p')
 #take the mean and standard deviation of training and validation error and pickle those results
-    err_out['test-error-mean' + str(cnt)] = cv_train.astype('float').mean(axis = 1)
-    err_out['test-error-std' + str(cnt)] = cv_train.astype('float').std(axis = 1)
-    err_out['train-error-mean' + str(cnt)] = cv_error.astype('float').mean(axis = 1)
-    err_out['train-error-std' + str(cnt)] = cv_error.astype('float').std(axis = 1)
+    err_out['train-error-mean' + str(cnt)] = cv_train.astype('float').mean(axis = 1)
+    err_out['train-error-std' + str(cnt)] = cv_train.astype('float').std(axis = 1)
+    err_out['valid-error-mean' + str(cnt)] = cv_valid.astype('float').mean(axis = 1)
+    err_out['valid-error-std' + str(cnt)] = cv_valid.astype('float').std(axis = 1)
     cv_tofile = pd.concat([cv_tofile, pd.DataFrame(err_out)], axis = 1)
-    pd.to_pickle(cv_tofile, 'cv_results/actions_e20/errors_search2.p')
+    pd.to_pickle(cv_tofile, 'cv_results/actions_e20/errors_search3.p')
     
 #output the parameters that were used
     df_params = df_params.append(p, ignore_index= True)
-    pd.to_pickle(df_params, 'cv_results/actions_e20/params_search2.p')
-    pd.to_pickle(pd.concat(cv_train, cv_error, axis = 1), 'cv_results/actions_e20/search2/res2.p')
+    pd.to_pickle(df_params, 'cv_results/actions_e20/params_search3.p')
