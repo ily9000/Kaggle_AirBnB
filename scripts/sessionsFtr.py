6c9cb9a3ba6b7d301c17d0d42fd6d14a143c46ae
Ubuntu <ubuntu@ip-172-31-32-50.us-west-2.compute.internal>
2016-02-07 01:50:38 +0000

    p4 sum feature

--- scripts/sessionsFtr.py
+++ scripts/sessionsFtr.py
@@ -9,18 +9,20 @@ sessionsDf = pd.read_csv('../data/sessions.csv')
 sessionsDf.dropna(subset=['user_id'], inplace = True)
 sessionsDf.fillna({'action_type': 'none', 'action': 'none','action_detail': 'none'}, inplace = True)
 
-#change these actions to match with training set
+#this action is unique to test set, we change it to match with training set
 sessionsDf.replace({'action': {'this_hosting_reviews_3000': 'this_hosting_reviews'}}, inplace = True)
 
 #merge these actions with action_detail
 #actions that correspond to message_post
 msgActs = sessionsDf.loc[sessionsDf.action_detail == 'message_post', 'action'].unique().tolist()
 #pending action & pending action detail: 'pending_pending' has a 1 to 1 relationship with booking request
+#merge impression & book so it has p4 in it's name, we will use this to sum all action details corresponding with p4
+#impression and book only have one action detail: p4, so there will be no loss
 #need to include click and patch
-actions = ['index', 'show', 'create', 'reviews', 'delete', 'recommendations', 'update', 'pending'] 
+actions = ['index', 'show', 'create', 'reviews', 'delete', 'recommendations', 'update', 'pending', 'impressions', 'book'] 
 actions += msgActs
 mergedActs = sessionsDf.action + '_' + sessionsDf.action_detail
-#ignoring patch
+#ignoring patch: action_detail is unknown in test set but has 2 addition ones in training set
 for a in actions:
     sessionsDf.loc[sessionsDf.action == a, 'action'] = mergedActs[sessionsDf.action == a]
 
@@ -31,18 +33,21 @@ a_ignore = set(train_actions) ^ set(test_actions)
         
 #build a dataframe with counts for each action
 #using device_type column to count the number of actions
-grouped_acts = sessionsDf[~sessionsDf.action.isin(a_ignore)].groupby(['user_id', 'action'])
+grouped_acts = sessionsDf.groupby(['user_id', 'action'])
 action_cnts = grouped_acts.apply(lambda x: x['device_type'].count())
 action_cnts = action_cnts.unstack(level = -1)
 action_cnts.fillna(0, inplace=True)
 
-#sum booking requests and message posts
-a_details = ['message_post', 'pending_pending|at_checkpoint']
+#sum booking requests, message posts, and p4
+#booking_request has 2 actions: pending and at_checkpoint
+a_details = ['message_post', 'pending_pending|at_checkpoint', 'p4']
 for a in a_details:
     action_cnts[a + 'total'] = action_cnts.loc[:, action_cnts.columns.str.contains(a)].sum(axis = 1)
-
+    
+action_cnts.drop(a_ignore, axis = 1, inplace = True)
+    
 #merge action counts with other sessions features, 
-#there should be no missing.
+#there should be no missing users.
 allDf = pd.DataFrame(index = sessionsDf.user_id.unique())
 allDf = pd.concat([allDf, action_cnts], axis = 1, join = 'outer')
 
